e3fe638fa9d4369d755d29a3334707688c1b4719
Theophilus Chidiebere Okoye <theophilusokoye6@gmail.com>
2021-02-06 09:47:47 +0200

    Initial Commit

---
+++ DesignCodePartOne/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCodePartOne
+//
+//  Created by plexie on 6/2/2021.
+//  Copyright © 2021 plexie. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
